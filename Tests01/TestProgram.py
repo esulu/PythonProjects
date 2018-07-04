a65323f192b849423aed5c8c2815ac0df8663d8e
esulu <esulu1@ocdsb.ca>
2018-07-04 14:02:17 -0400

    Add files via upload

---
+++ Tests01/TestProgram.py
@@ -0,0 +1,15 @@
+# Output to the console
+print("Hello World!\nHello Again!")
+# Power
+print(2**10)
+# Floor and mod
+print(25 % (15 // 4))
+# String multiplication
+print("\nHello World" * 3)
+# User input
+entry = input("What is your name: ")
+print("Hello " + entry + ", nice to meet you.")
+# User float input
+# print(float(input("Enter a number: ")) / int(input("Enter another number: ")))
+x = 5
+print(str(x) + "2")
