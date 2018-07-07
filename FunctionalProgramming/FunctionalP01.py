cd3d6a1af4d6849fcc9096560f8f16698814c289
esulu <erensulu11@gmail.com>
2018-07-07 16:08:57 -0400

    Add files via upload

--- FunctionalProgramming/FunctionalP01.py
+++ FunctionalProgramming/FunctionalP01.py
@@ -9,3 +9,11 @@ def add_five(x):
 
 # Prints 20
 print(apply_twice(add_five, 10))
+
+# Named function
+def polynomial(x):
+    return x**2 + 5*x + 4
+print(polynomial(-4))
+
+# Lambda (anonymous) function
+print((lambda x: x**2 + 5*x + 4)(-4))
