fd1eef02f77e6333e10c0e52c6a0ae854d423d09
esulu <erensulu11@gmail.com>
2018-08-08 10:06:43 -0400

    Create Metacharacters.py

---
+++ RegularExpressions/Metacharacters.py
@@ -0,0 +1,18 @@
+# Metacharacters in Python
+
+# Metacharacters are what make regular expressions more powerful than normal string methods
+
+# . (dot) : matches any character, other than a new line
+
+import re
+
+pattern = r"gr.y"
+
+if re.match(pattern, "grey"):
+    print("Match 1")
+
+if re.match(pattern, "gray"):
+    print("Match 2")
+
+if re.match(pattern, "blue"):
+    print("Match 3")
