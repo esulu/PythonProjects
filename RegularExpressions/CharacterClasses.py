139b2a601f3966877e5b2c1efa8c8217907dcf02
esulu <erensulu11@gmail.com>
2018-08-08 10:58:23 -0400

    Update CharacterClasses.py

--- RegularExpressions/CharacterClasses.py
+++ RegularExpressions/CharacterClasses.py
@@ -1,1 +1,5 @@
 # Character classes in Python
+
+# Character classes provide a way to match only one of a specific set of characters
+
+# A character class is created by putting the characters it matches in square brackets
