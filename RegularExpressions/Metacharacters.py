89a6d0d5ceea9e9ade411814dd20b973b8d3cb98
esulu <erensulu11@gmail.com>
2018-08-08 10:08:50 -0400

    Update Metacharacters.py

--- RegularExpressions/Metacharacters.py
+++ RegularExpressions/Metacharacters.py
@@ -16,3 +16,16 @@ if re.match(pattern, "gray"):
 
 if re.match(pattern, "blue"):
     print("Match 3")
+
+# The metacharacters ^ and $ match the start and end of a string, respectively
+
+pattern = r"^gr.y$"
+
+if re.match(pattern, "grey"):
+    print("Match 1")
+
+if re.match(pattern, "gray"):
+    print("Match 2")
+
+if re.match(pattern, "stingray"):
+    print("Match 3")
