3e235c9f2cd776c0fe4ddc1f993b9265c6edb65e
esulu <erensulu11@gmail.com>
2018-08-08 11:07:12 -0400

    Update CharacterClasses.py

--- RegularExpressions/CharacterClasses.py
+++ RegularExpressions/CharacterClasses.py
@@ -3,3 +3,16 @@
 # Character classes provide a way to match only one of a specific set of characters
 
 # A character class is created by putting the characters it matches in square brackets
+
+import re
+
+pattern = r"[aeiou]"
+
+if re.search(pattern, "grey"):
+    print("Match 1")
+
+if re.search(pattern, "qwertyuiop"):
+    print("Match 2")
+
+if re.search(pattern, "rhythm myths"):
+    print("Match 3")
