4480d771dfbeb04e0e381d134a7beccc452de002
esulu <erensulu11@gmail.com>
2018-08-13 13:57:30 -0400

    Update Metacharacters2.py

--- RegularExpressions/Metacharacters2.py
+++ RegularExpressions/Metacharacters2.py
@@ -60,3 +60,20 @@ if re.match(pattern, "999"):
 
 if re.match(pattern, "9999"):
     print("Match 3")
+
+# Another important metacharacter is |
+# This means "or"
+
+pattern = r"gr(a|e)y"
+
+match = re.match(pattern, "gray")
+if match:
+    print("Match 1")
+
+match = re.match(pattern, "grey")
+if match:
+    print("Match 2")
+
+match = re.match(pattern, "griy")
+if match:
+    print("Match 3")
