55a6c4433f88cdb6ccc4f1559ec21c747c3f085c
esulu <erensulu11@gmail.com>
2018-08-13 11:31:15 -0400

    Update Groups.py

--- RegularExpressions/Groups.py
+++ RegularExpressions/Groups.py
@@ -1,1 +1,16 @@
 # Introduction to groups in Python
+
+# Created by surrounding part of a regular expression with ()'s
+
+import re
+
+pattern = r"egg(spam)*"
+
+if re.match(pattern, "egg"):
+    print("Match 1")
+
+if re.match(pattern, "eggspamspamspamegg"):
+    print("Match 2")
+
+if re.match(pattern, "spam"):
+    print("Match 3")
