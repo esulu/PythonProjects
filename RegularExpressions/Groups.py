1d02b11c6997fde5bad758f5bd856a62df4d41aa
esulu <erensulu11@gmail.com>
2018-08-13 13:54:19 -0400

    Update Groups.py

--- RegularExpressions/Groups.py
+++ RegularExpressions/Groups.py
@@ -18,3 +18,8 @@ if re.match(pattern, "spam"):
 # (spam) represents a  group in the example pattern shown above
 
 # Example: '([^aeiou][aeiou][^aeiou])+' would match one or more repetitions of a non-vowel, a vowel and a non-vowel
+
+# The content of groups in a match can be accessed using the <group> function
+# group(0) / group() returns the whole match
+# group(n) where n is larger than 0, returns the nth group from the left
+# groups() returns all groups up from 1
