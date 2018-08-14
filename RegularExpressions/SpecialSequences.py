684d282d4348609388d951fe28d8fd22f2c91c68
esulu <erensulu11@gmail.com>
2018-08-14 11:34:13 -0400

    Update SpecialSequences.py

--- RegularExpressions/SpecialSequences.py
+++ RegularExpressions/SpecialSequences.py
@@ -1,1 +1,6 @@
 # Introduction to special sequences in Python
+
+# Syntax: \<character>
+
+# One useful special sequence is a backslash and a number between 1 and 99, e.g., \1 or \17
+# This matches the expression of the group of that number
