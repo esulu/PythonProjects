234b59d598dedbf88cf60bd9cdb69e25f7b3c329
esulu <erensulu11@gmail.com>
2018-08-14 11:47:20 -0400

    Update SpecialSequences.py

--- RegularExpressions/SpecialSequences.py
+++ RegularExpressions/SpecialSequences.py
@@ -4,3 +4,19 @@
 
 # One useful special sequence is a backslash and a number between 1 and 99, e.g., \1 or \17
 # This matches the expression of the group of that number
+
+import re
+
+pattern = r"(.+) \1"
+
+match = re.match(pattern, "word word")
+if match:
+    print("Match 1")
+
+match = re.match(pattern, "?! ?!")
+if match:
+    print("Match 2")
+
+match = re.match(pattern, "abc def")
+if match:
+    print("Match 3")
