29914e7c2749818f8f9ea6590f628107ad1e3d63
esulu <erensulu11@gmail.com>
2018-08-14 12:10:24 -0400

    Update SpecialSequences.py

--- RegularExpressions/SpecialSequences.py
+++ RegularExpressions/SpecialSequences.py
@@ -51,3 +51,19 @@ if match:
 # \A and \Z match the beginning and the end of a string respectively
 # \b matches the empty string between \w and \W characters, or \w characters and the beginning or end of the string
 # \B matches the empty string anywhere else
+
+pattern = r"\b(cat)\b"
+
+match = re.search(pattern, "The cat sat!")
+if match:
+    print("Match 1")
+
+match = re.search(pattern, "We s>cat<tered?")
+if match:
+    print("Match 2")
+
+match = re.search(pattern, "We scattered?")
+if match:
+    print("Match 3")
+
+# <\b(cat)\b> matches the word "cat" surrounded by word boundaries
