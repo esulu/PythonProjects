ee4f14491ac32b8a00fc8149da3578a966f2947c
esulu <erensulu11@gmail.com>
2018-08-14 11:52:53 -0400

    Update SpecialSequences.py

--- RegularExpressions/SpecialSequences.py
+++ RegularExpressions/SpecialSequences.py
@@ -23,3 +23,10 @@ if match:
 
 # Note: <(.+) /1> != <(.+)(.+)> because /1 refers to the first group's subexpression, which is the matched expression
 # itself, and not the regex pattern
+
+# More useful special sequences are \d, \s, and \w
+# These match digits, whitespace, and word characters respectively
+
+# ASCII equivalent: [0-9], [\t\n\r\f\v], and [a-zA-Z0-9_]
+# The upper case version of the special sequences mean the opposite of the lower case versions
+# Ex: /D matches anything that isn't a digit
